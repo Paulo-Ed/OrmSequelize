27b422e6e807afd71b30ad77c2df88dbbb54700d
Paulo Santos <pauloed_@outlook.com>
2021-12-02 11:58:35 -0300

    Criando métodos crud para a entidade pessoa

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -26,6 +26,45 @@ class PessoaController {
             return res.status(500).json(error.message)
        }
     } 
+    //Adiciona Registro de Pessoa
+    static async criaPessoa(req, res){
+        const novaPessoa = req.body
+        try {
+            const novaPessoaCriada = await dataBase.Pessoas.create(novaPessoa)
+            return res.status(200).json(novaPessoaCriada)
+        }
+        catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
+    //Atualiza Registro de Pessoa
+    static async atualizaPessoa(req, res){
+        const { id } = req.params
+        const novasInformacoes = req.body
+        try {
+            await dataBase.Pessoas.update(novasInformacoes, {
+                where: {
+                    id: Number(id)
+                }
+            })
+            const pessoaAtualizada = await dataBase.Pessoas.findOne({where: { id: Number(id)}})
+            return res.status(200).json(pessoaAtualizada)
+        }
+        catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
+    //Apaga Registro de Pessoa
+    static async apagaPessoa(req, res){
+        const { id } = req.params
+        try {
+            await dataBase.Pessoas.destroy({where: { id: Number(id) }})
+            return res.status(200).json(`Registro ${id} Apagado com Sucesso`)
+        }
+        catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
 }
 
 module.exports = PessoaController
--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -3,6 +3,18 @@ const PessoaController = require('../controllers/PessoaController')
 
 const router = Router()
 
+//Retorna todas as pessoas
 router.get('/pessoas', PessoaController.retornaTodasAsPessoas)
+
+//Retorna uma pessoa com o id informado
 router.get('/pessoas/:id', PessoaController.retornaUmaPessoa)
+
+//Adiciona uma nova pessoa
+router.post('/pessoas', PessoaController.criaPessoa)
+
+//Atualiza o registro de uma pessoa
+router.put('/pessoas/:id', PessoaController.atualizaPessoa)
+
+//Deleta o registro de uma pessoa
+router.delete('/pessoas/:id', PessoaController.apagaPessoa)
 module.exports = router
