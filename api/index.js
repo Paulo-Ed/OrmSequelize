e3c0b2483f253892ff64dabb7bcd970b1f417974
Paulo Santos <pauloed_@outlook.com>
2021-12-02 11:04:34 -0300

    separando as atribuiçoes da api

--- api/index.js
+++ api/index.js
@@ -1,17 +1,18 @@
 //Importando o express para iniciar o servidor local
 const express = require('express')
-const bodyParser = require('body-parser')
+const routes = require('./routes')
 
 //Inicializando o servidor
 const app = express()
 
-app.use(bodyParser.json())
-
 //Definindo porta
 const port = 3000
 
+//Chamando o módulo routes
+routes(app)
+
 //Criando rota inicial
-app.get('/teste', (req, res) => res.status(200).send({mensagem: 'Bem vindo a Api'}))
+//app.get('/teste', (req, res) => res.status(200).send({mensagem: 'Bem vindo a Api'}))
 
 //Definindo conexão com o servidor
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`))
